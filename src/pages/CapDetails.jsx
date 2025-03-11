75f0c62c454af0c5061f010280d89cdcbca45e03
atikfaisalemon <atikfaisalemon@gmail.com>
2025-03-12 01:23:32 +0600

    feat scaleton

--- src/pages/CapDetails.jsx
+++ src/pages/CapDetails.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import AddToCart from "./AddToCart";
+
 const CapDetails = () => {
   const { id } = useParams(); // Get cap ID from URL
   const [cap, setCap] = useState();
@@ -14,10 +15,8 @@ const CapDetails = () => {
           `${import.meta.env.VITE_BASE_API_URL}/api/products/${id}?populate=*`
         );
         const { data } = await response.json();
-        console.log("first", data);
         setCap(data);
-
-        setMainImage(cap?.images[0].formats.large.url);
+        setMainImage(data?.images[0].formats.large.url);
         setLoading(false);
       } catch (error) {
         console.error("Error fetching products:", error);
@@ -27,17 +26,40 @@ const CapDetails = () => {
 
     fetchProducts();
   }, [id]);
+
   useEffect(() => {
     if (cap?.images && cap.images.length > 0) {
       setMainImage(cap.images[0].formats.large.url);
     }
   }, [cap]);
-  console.log("params", id);
+
+  if (loading) {
+    return (
+      <div className="pt-24">
+        <div className="container mx-auto p-6 flex flex-col md:flex-row md:justify-between md:gap-12 mb-36">
+          <div className="gap-12 flex flex-col md:flex-row">
+            <div className="flex md:items-start justify-center items-center gap-2 mt-4 flex-row md:flex-col order-1 md:order-none">
+              <div className="md:w-24 md:h-24 w-15 h-15 bg-gray-200 animate-pulse rounded-md"></div>
+              <div className="md:w-24 md:h-24 w-15 h-15 bg-gray-200 animate-pulse rounded-md"></div>
+              <div className="md:w-24 md:h-24 w-15 h-15 bg-gray-200 animate-pulse rounded-md"></div>
+            </div>
+            <div className="flex justify-center order-0 md:order-1">
+              <div className="md:w-[600px] md:h-[800px] h-90 w-90 bg-gray-200 animate-pulse rounded-2xl md:rounded-[120px]"></div>
+            </div>
+          </div>
+          <div className="max-w-[400px]">
+            <div className="md:text-2xl text-xl font-bold mb-4 mt-3 bg-gray-200 animate-pulse h-8 w-3/4"></div>
+            <div className="mt-4 bg-gray-200 animate-pulse h-20 w-full"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   if (!cap) {
     return <h2 className="text-center text-red-500">Cap not found!</h2>;
   }
-  console.log("first", cap);
+
   return (
     <div>
       {/* for desktop mode */}
